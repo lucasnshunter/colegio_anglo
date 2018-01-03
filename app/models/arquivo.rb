419e7d21d87ab57763793d1a121c39202abfa26c
lucas nunes de sousa <lucasnshunter@hotmail.com>
2018-01-03 09:58:57 +0000

    problema da definição das fotos do roda pé e cabeçalho resolvidos

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -3,6 +3,6 @@ class Arquivo < ActiveRecord::Base
      has_attached_file :arquivo
     
     #validates_attachment :arquivo, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
-   validates_attachment_content_type :arquivo, :content_type => %w(application/zip application/msword application/vnd.ms-office application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
-
+    validates_attachment_content_type :arquivo, :content_type => %w(application/zip application/msword application/vnd.ms-office application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
+    
 end
