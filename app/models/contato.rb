53d777185990f784d2fb074a6355732298c13d59
lucas nunes de sousa <lucasnshunter@hotmail.com>
2018-02-05 20:05:05 +0000

    s

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,9 +1,9 @@
 class Contato < ActiveRecord::Base
      has_attached_file :arquivo, styles: { medium: "300x300>", thumb: "100x100>" }
      validates_attachment_content_type :arquivo, :content_type => ["application/pdf", "application/zip", "application/x-zip", "application/x-zip-compressed","application/octet-stream","application/vnd.ms-office","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.presentationml.presentation","image/jpg", "image/png", "image/jpeg","application/msword","application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
-     validates_presence_of :nome
-     validates_presence_of :email
-     validates_presence_of :text
+     #validates_presence_of :nome
+     #validates_presence_of :email
+     #validates_presence_of :text
 
 end
 
