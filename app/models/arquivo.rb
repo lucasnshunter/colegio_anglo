9e64d1db945420d598ce0deaf084c9eb4cfdbbbc
lucas nunes de sousa <lucasnshunter@hotmail.com>
2018-02-05 13:20:02 +0000

    formtaos de aquivos

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -3,6 +3,8 @@ class Arquivo < ActiveRecord::Base
      has_attached_file :arquivo
     
      has_attached_file :document
-    
-     validates_attachment :arquivo, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+    has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :arquivo, :content_type => ["application/pdf", "application/zip", "application/x-zip", "application/x-zip-compressed","application/octet-stream","application/vnd.ms-office","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.presentationml.presentation","image/jpg", "image/png", "image/jpeg","application/msword","application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
+    #validates_attachment :arquivo, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
+    
