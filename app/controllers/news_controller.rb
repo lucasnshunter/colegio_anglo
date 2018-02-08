51a14a43ed8bfbadf55c34a58301fd6692474b43
lucas nunes de sousa <lucasnshunter@hotmail.com>
2018-02-08 16:12:44 +0000

    s

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
    before_action :authenticate_user!
-    layout'dashboard'
+    layout'noticias'
   before_action :set_news, only: [:show, :edit, :update, :destroy]
 
   # GET /news
