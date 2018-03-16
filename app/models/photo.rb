262c886f9fb9583b1427e89469061de5891c47d0
lucas nunes de sousa <lucasnshunter@hotmail.com>
2018-03-16 19:09:32 +0000

    s

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ActiveRecord::Base
      has_attached_file :foto, styles: { medium: "300x300>", thumb: "100x100>" }
       validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
       belongs_to :galery
-      self.per_page = 6
+      self.per_page = 2
 
 end
